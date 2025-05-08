d776a08cfb598f0418d4c232836cd62631fd969f
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-08 17:17:57 +0100

    feat: Repository de Category adicionado

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,16 @@
 import { Router } from 'express';
 
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_At: new Date(),
-  });
-
-  categories.push(category);
-
-  response.status(201).json({ category });
+  response.status(201).send();
 });
 
 export { categoriesRoutes };
