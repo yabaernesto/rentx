85ab8d0ae132fd2e048985b356cf93f0dc81bbb0
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-08 17:47:07 +0100

    feat: Validando cadastro de Categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,15 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category Already Exists!' });
+  }
+
   categoriesRepository.create({ name, description });
 
-  response.status(201).send();
+  return response.status(201).send();
 });
 
 categoriesRoutes.get('/', (request, response) => {
   const all = categoriesRepository.list();
 
-  response.json(all);
+  return response.json(all);
 });
 
 export { categoriesRoutes };
