1677adf1961b62e5bff304c8818d0d1d7ae1960d
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-12 03:23:41 +0100

    refactor: refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running. Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running! Port: ${PORT}`));
