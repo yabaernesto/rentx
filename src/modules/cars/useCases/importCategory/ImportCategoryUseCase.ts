925ae501b429f4387b0bb6814a40afaf7edbac43
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-14 17:49:29 +0100

    feat: lendo dados com streams

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,21 @@
+import fs from 'node:fs';
+import { parse } from 'csv-parse';
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File | undefined): void {
-    console.log(file);
+    if (!file) {
+      throw new Error();
+    }
+
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 
