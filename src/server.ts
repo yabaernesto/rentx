08c3d9bea521ef89dfcfbea00338102db69f677d
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-21 00:33:20 +0100

    chore: TypeORM e Docker Compose

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
 import swaggerFile from './swagger.json';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -14,4 +15,14 @@ app.use(router);
 
 const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running! Port: ${PORT}`));
+AppDataSource.initialize()
+  .then(() => {
+    console.log('📦 Banco de dados conectado com sucesso!');
+
+    app.listen(PORT, () =>
+      console.log(`Server is running on port: ${PORT} 🚀`),
+    );
+  })
+  .catch((error) => {
+    console.error('❌ Error:', error);
+  });
