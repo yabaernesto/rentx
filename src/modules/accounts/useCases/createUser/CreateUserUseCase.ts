49892ba3154e85e5ac90bc69998c5b7b0cac5c33
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-23 23:44:20 +0100

    feat: Use Case de User

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
