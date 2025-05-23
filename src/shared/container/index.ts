9f4e69357362e567f4ed2102e4dd8d5bf1da36da
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-23 23:45:40 +0100

    feat: container de injeção de dependeria de register

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,13 @@
 import { container } from 'tsyringe';
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/repositories/implementation/CategoriesRepository';
 
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementation/SpecificationsRepository';
+
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementation/UsersRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -14,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
