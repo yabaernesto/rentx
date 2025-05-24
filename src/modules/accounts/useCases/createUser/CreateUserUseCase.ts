2a4d998ab1b900ebb147b7577ea77a5779fe4b1d
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-24 02:16:37 +0100

    feat: Criptografar senha

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { hash } from 'bcrypt';
 
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
@@ -12,16 +13,22 @@ class CreateUserUseCase {
 
   async execute({
     name,
-    username,
     email,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const password_hash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      username,
       email,
-      password,
+      password: password_hash,
       driver_license,
     });
   }
