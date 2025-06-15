b8586780b27a1ffed89c9257aadbd4e7cd7bf3a1
Yaba Ernesto <yabaernesto@gmail.com>
2025-06-15 01:48:11 +0100

    chore: aliases import

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,6 +1,6 @@
 import { Repository } from 'typeorm';
 
-import { Specification } from '../../entities/Specification';
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 import {
   ICreateSpecificationDTO,
   ISpecificationsRepository,
