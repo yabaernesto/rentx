36414ee82de86b0ae9b6c034fb8cd813b045306f
Yaba Ernesto <yabaernesto@gmail.com>
2025-06-18 00:18:29 +0100

    test: Use Case of Car

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,3 +1,5 @@
+import { AppError } from '@shared/errors/AppError';
+
 import { CreateCarUseCase } from './CreateCarUseCase';
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
 
@@ -11,7 +13,7 @@ describe('Create Car', () => {
   });
 
   it('should be able to create a new car', async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: 'Name car',
       description: 'Description car',
       daily_rate: 100,
@@ -20,5 +22,45 @@ describe('Create Car', () => {
       brand: 'Brand',
       category_id: 'category',
     });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able to create a car with exists license plate', async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car1',
+        description: 'Description car',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'category',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car2',
+        description: 'Description car',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'category',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Available',
+      description: 'Description car',
+      daily_rate: 100,
+      license_plate: 'ABCD-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+
+    expect(car.available).toBe(true);
   });
 });
