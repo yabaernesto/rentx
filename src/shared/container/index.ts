0f4391292d6e396695850253d3ee6d2a3b1ad404
Yaba Ernesto <yabaernesto@gmail.com>
2025-06-18 01:02:18 +0100

    feat: add Car Controller

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,9 @@ import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositori
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
+
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
   CategoriesRepository,
@@ -23,3 +26,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
-import { categoriesRoutes } from './categories.routes';
-import { specificationsRoutes } from './specifications.routes';
+import { carsRoutes } from './cars.routes';
 import { usersRoutes } from './users.routes';
+import { categoriesRoutes } from './categories.routes';
 import { authenticateRoutes } from './authenticate.routes';
+import { specificationsRoutes } from './specifications.routes';
 
 const router = Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
+router.use('/cars', carsRoutes);
 router.use(authenticateRoutes);
 
 export { router };
