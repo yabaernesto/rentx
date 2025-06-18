3a702003f8faa236a31d5e67a169d1c25eedc940
Yaba Ernesto <yabaernesto@gmail.com>
2025-06-18 01:46:19 +0100

    feat: add Entity Car

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { User } from '@modules/accounts/infra/typeorm/entities/User';
 import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
@@ -14,7 +15,7 @@ export const AppDataSource = new DataSource({
   database: 'rentx',
   synchronize: true,
   logging: false,
-  entities: [Category, Specification, User],
+  entities: [Category, Specification, User, Car],
   migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
   subscribers: [],
 });
